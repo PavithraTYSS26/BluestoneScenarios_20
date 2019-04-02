1373b8f85248de9a3ff1675fe17797e6a2687361
PavithraTYSS26 <pavithraoptimist@gmail.com>
2019-04-02 14:19:43 +0530

    Hey pavi do properly.........

---
+++ src/selenium/git/DemoClass.java
@@ -0,0 +1,12 @@
+package selenium.git;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hai pavithra this is demo class");
+
+	}
+
+}
